e42e8e5eec01cc930f32de6b7a27053c6211fe88
gconstanti2 <golden_name2005@yahoo.com>
2017-10-21 16:09:47 +0100

    test commit

--- connection-gabriel.py
+++ connection-gabriel.py
@@ -27,18 +27,20 @@ class Market_Maker:
             "XLF": 100, "VALBZ": 10, "VALE": 10}
         self._inventory = {"XLF": 0, "VALE": 0, "VALBZ": 0, "GS": 0, \
             "BOND": 0, "MS": 0,  "WFC": 0}
-        self._buy = {"XLF": "", "GS": "", "MS": "", "WFC": "", "BOND": "", \
-            "VALE": "", "VALBZ": ""}
-        self._sell = {"XLF": "", "GS": "", "MS": "", "WFC": "", "BOND": "", \
-            "VALE": "", "VALBZ": ""}
+        self._buy = {"XLF": {}, "GS": {}, "MS": {}, "WFC": {}, "BOND": {}, \
+            "VALE": {}, "VALBZ": {}}
+        self._sell = {"XLF": {}, "GS": {}, "MS": {}, "WFC": {}, "BOND": {}, \
+            "VALE": {}, "VALBZ": {}}
         self._buyprices = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
             "VALE":0, "VALBZ":0}
         self._sellprices = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
             "VALE":0, "VALBZ":0}
+        self._spreads = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
+            "VALE":0, "VALBZ":0}
         self._quant = 5
 
-        self._cancels = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
-            "BOND": [], "VALE": [], "VALBZ": []}
+        self._cancels = {"XLF": {}, "GS": {}, "MS": {}, "WFC": {}, \
+            "BOND": {}, "VALE": {}, "VALBZ": {}}
 
         self._buyidx = 1
         self._sellidx = 100000
@@ -47,87 +49,114 @@ class Market_Maker:
             "BOND": [], "VALE": [], "VALBZ": []}
         self._curr_asks = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
             "BOND": [], "VALE": [], "VALBZ": []}
+        self._pending = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
+            "VALE":0, "VALBZ":0}
 
 
-    def update_inventory(self, fill_strings):
+    def update_inventory(self, fill_dict):
         ## go over the fill strings, delete the corresponding outstanding bids
-        for _ in fill_strings:
-            name, ord, stock, action = fill_strings[_].split()[0:4]
-            ord = int(ord)
-            if name == 'FILL' and action == 'BUY':
-                to_delete = []
-                # go over the bids and asks and cancel that one
-                for idx in range(len(self._curr_bids[stock]))
-                    if self._curr_bids[stock][idx][0] == ord
-                    to_delete.append(idx)
-                del self._curr_bids[stock][to_delete] # delte filled bids
-                self._inventory[stock] += self._quant
+        action = fill_dict['dir']
+        ord = fill_dict['order_id']
+        stock = fill_dict['symbol']
+        loc_quant = fill_dict['size']
+        if action == 'BUY':
+            to_delete = []
+            # go over the bids and asks and cancel that one
+            for idx in range(len(self._curr_bids[stock]))
+                if self._curr_bids[stock][idx][0] == ord
+                to_delete.append(idx)
+            del self._curr_bids[stock][to_delete] # delte filled bids
+            self._inventory[stock] += loc_quant
 
-            if name == 'FILL' and action == 'SELL':
-                to_delete = []
-                # go over the bids and asks and cancel that one
-                for idx in range(len(self._curr_asks[stock]))
-                    if self._curr_asks[stock][idx][0] == ord
-                    to_delete.append(idx)
-                del self._curr_asks[stock][to_delete] # delete filled asks
-                self._inventory[stock] -= self._quant # 
+        if action == 'SELL':
+            to_delete = []
+            # go over the bids and asks and cancel that one
+            for idx in range(len(self._curr_asks[stock]))
+                if self._curr_asks[stock][idx][0] == ord
+                to_delete.append(idx)
+            del self._curr_asks[stock][to_delete] # delete filled asks
+            self._inventory[stock] -= loc_quant # 
 
-         
-    def get_avg_prices(self, books):
+    def get_class_prices(self, stock)
+        return self._buyprices[stock], self._sellprices[stock], \
+            self._spreads[stock]
+
+    def get_avg_prices(self, book):
         # "book" is the book dictionary for this collection of stocks
-        for _ in books:
-            key = books[_]['symbol']
+        key = book['symbol']
 
-            self._buyprices[key] = 0.0
-            length = len(books['buy'])
-            for idx in range(length):
-                self._buyprices[key] += books[_]['buy'][idx][0]
-            self._buyprices[key] //= length
+        buyprices = 0.0
+        length = len(books['buy'])
+        for idx in range(length):
+            buyprices += book['buy'][idx][0]
+        buyprices //= length
 
-            self._sellprices[key] = 0.0
-            length = len(books['sell'])
-            for idx in range(length):
-                self._sellprices[key] += books[_]['sell'][idx][0]
-            self._sellprices[key] //= length
+        sellprices = 0.0
+        length = len(books['sell'])
+        for idx in range(length):
+            sellprices += book['sell'][idx][0]
+        sellprices //= length
+        return buyprices, sellprices
 
-    def purge(self):
-        for _ in self._stocks:
-            self._cancels[_] = []
-            # check if there are any existing stocks with outstanding
-            # bids or asks and cancel them
-            for order in self._curr_bids[_]:
-                self.cancels[_].append("CANCEL " + str(order[0]))
-            for order in self._curr_asks[_]:
-                self.cancels[_].append("CANCEL " + str(order[0]))
+    def update_avg_prices(self, book):
+        # "book" is the book dictionary for this collection of stocks
+        key = book['symbol']
 
-            self._curr_bids = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
-                "BOND": [], "VALE": [], "VALBZ": []}
-            self._curr_asks = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
-                "BOND": [], "VALE": [], "VALBZ": []}
+        self._buyprices[key] = 0.0
+        length = len(books['buy'])
+        for idx in range(length):
+            self._buyprices[key] += book['buy'][idx][0]
+        self._buyprices[key] //= length
 
-        return self._cancels
+        self._sellprices[key] = 0.0
+        length = len(books['sell'])
+        for idx in range(length):
+            self._sellprices[key] += book['sell'][idx][0]
+        self._sellprices[key] //= length
+        self._spreads = [self._sellprices[i] - self._buyprices[i] for i in
+            self.sellprices]
 
+    def purge(self, _):
+        self._cancels[_] = {}
+        # check if there are any existing stocks with outstanding
+        # bids or asks and cancel them
+        for order in self._curr_bids[_]:
+            self.cancels[_] = {"type": "CANCEL", "order_id": order[0]}
+        for order in self._curr_asks[_]:
+            self.cancels[_] = {"type": "CANCEL", "order_id": order[0]}
 
-    def update_orders(self): # dictionary of prices
+        self._curr_bids = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
+            "BOND": [], "VALE": [], "VALBZ": []}
+        self._curr_asks = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
+            "BOND": [], "VALE": [], "VALBZ": []}
+        self._pending = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
+        "VALE":0, "VALBZ":0}
+        return self._cancels[_]
 
-        for _ in self._stocks: # stock key
-            self._buyidx += 1
-            self._sellidx += 1
-            # update buy and sell orders
-            self._buy[_] = "ADD " + self._buyidx + " " + key + " BUY " + \
-                str(sell._buyprices[_])+" " + self._quant
-            self._sell[_] = "ADD " + self.buyidx + " " + key + " SELL " + \
-                str(sell._sellprices[_])+" " + self._quant
+    def get_pending(self, stock)
+        return self._pending[stock]
 
-             if self._inventory[_] < self._limits[_] - self._quant:
-                 self._curr_bids[_].append([self._buyidx, sell._buyprices[_], \
-                     self._quant])
-             if self._inventory[_] > -self._limits[_] + self._quant
-                 self._curr_asks[_].append([self._sellidx, sell._sellprices[_],\
-                     self._quant])
+    def update_orders(self, _): # dictionary of prices
+        self._buyidx += 1
+        self._sellidx += 1
+        # update buy and sell orders
+        self._buy[_] = {"type": "ADD", "order_id": self._buyidx, \
+            "symbol": _, "dir": "BUY", "price": self._buyprices[_], \
+            "size": self._quant}
+        self._sell[_] = {"type": "ADD", "order_id": self._sellidx, \
+            "symbol": _, "dir": "SELL", "price": self._sellprices[_], \
+            "size": self._quant}
 
-        return self._buy,self._sell
+        if self._inventory[_] < self._limits[_] - self._quant and 
+           self._inventory[_] > -self._limits[_] + self._quant:
 
+            self._curr_bids[_].append([self._buyidx, sell._buyprices[_], \
+                self._quant])
+            self._curr_asks[_].append([self._sellidx, sell._sellprices[_],\
+                self._quant])
+            self._pending = 1
+            return self._buy, self._sell[_]
+         return None
 
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
@@ -162,6 +191,8 @@ def read_from_exchange(exchange):
 
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
+MM = MarketMaker()
+
 
 def main():
     from random import random
@@ -173,11 +204,30 @@ def main():
     # exponential explosion in pending messages. Please, don't do that!
     while True:
         msg = read_from_exchange(exchange)
-        if msg["type"] == "ack":
-            print("The exchange replied:", msg, file=sys.stderr)
-            n += 4
-            write_to_exchange(exchange, {"type": "add", "order_id": n+2, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 20})
-            write_to_exchange(exchange, {"type": "add", "order_id": n+3, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 20})
+        if msg["type"] == "book":
+            status = MM.get_pending(msg['symbol'])
+            if status: # if pending, see if we need to purge and restart:
+                buyprice, sellprice = MM.get_avg_prices(msg)
+                class_buyprice, class_sellprice, class_spread = \
+                    MM.get_class_prices(msg['symbol'])
+                 
+                purge_flag = 0   
+                if abs((buyprice - sellprice) - \
+                    (class_buyprice - class_sellprice)) >= class_spread:
+                    purge_flag = 1
+
+                if purge_flag: # if we purge, reset status:
+                    order = MM.purge(msg['symbol'])
+                    write_to_exchange(exchange, order)
+
+            if not status: # if not pending start a new trade
+                MM.update_avg_prices(msg)
+                orders = MM.update_orders(msg['symbol'])
+                if orders is not None:
+                    write_to_exchange(exchange, orders[0])
+                    write_to_exchange(exchange, orders[1])
+        if msg["type"] == "fill":
+            update_inventory(msg)
 
 if __name__ == "__main__":
     main()
