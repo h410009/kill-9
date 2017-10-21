3252948ed8db2f96c945cfd2060004ec2628dffa
h410009 <h410009@gmail.com>
2017-10-21 17:11:25 +0100

    a

---
+++ dev2.py
@@ -0,0 +1,9 @@
+class Portfolio():
+    def __init__(self):
+        self.positions = {"GS": 0, "MS": 0, "WFC": 0, "XLF": 0, "VALE": 0, "VALBZ": 0}
+        self.highestBuy = {'GS':0, 'MS':0, 'WFC':0, 'XLF':0, 'VALBZ':0, 'VALE':0, 'BOND':0}
+        self.cheapestSell = {'GS':0, 'MS':0, 'WFC':0, 'XLF':0, 'VALBZ':0, 'VALE':0, 'BOND':0}
+        self.orders = {}
+        self.symbolToLimit = {'BOND':100, 'GS': 100, 'MS': 100, 'WFC': 100, 'XLF': 100, 'VALBZ': 10, 'VALE': 10}
+
+    
