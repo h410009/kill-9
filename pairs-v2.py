f88c8f7a3db8176b67a83d7666d96da0ff5b24f0
gconstanti2 <golden_name2005@yahoo.com>
2017-10-21 14:59:27 +0100

    test commit ETF

--- pairs-v2.py
+++ pairs-v2.py
@@ -63,40 +63,59 @@ class PairsTrading(MovAvg):
         for _ in self._pairs:
             ?????????
 """
-def split (buy, sell):
-    buy_tuples = [tuple(x) for x in buy]
-    sell_tuples = [tuple(x) for x in sell]
-    min_buy_price, min_buy_quant = min(buy_tuples)
-    min_sell_price, min_sell_quant = min(sell_tuples)
-    max_buy_price, max_buy_quant = max(buy_tuples)
-    max_sell_price, max_sell_quant = max(sell_tuples)
-
-
 class Market_Maker:
     def __init__(self):
+        self._stocks = ["BOND", "GS", "MS", "WFC", "XLF", "VALBZ", "VALE"]
         self._limits = {"BOND": 100, "GS": 100, "MS": 100, "WFC": 100, \
             "XLF": 100, "VALBZ": 10, "VALE": 10}
         self._inventory = {"XLF": 0, "VALE": 0, "VALBZ": 0, "GS": 0, \
             "BOND": 0, "MS": 0,  "WFC": 0}
-        self._buy = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
-            "VALE":0, "VALBZ": 0}
-        self._sell = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
-            "VALE":0, "VALBZ": 0}
+        self._buy = {"XLF": "", "GS": "", "MS": "", "WFC": "", "BOND": "", \
+            "VALE": "", "VALBZ": ""}
+        self._sell = {"XLF": "", "GS": "", "MS": "", "WFC": "", "BOND": "", \
+            "VALE": "", "VALBZ": ""}
         self._buyprices = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
             "VALE":0, "VALBZ":0}
         self._sellprices = {"XLF": 0, "GS": 0, "MS": 0, "WFC": 0, "BOND": 0, \
             "VALE":0, "VALBZ":0}
+        self._quant = 5
 
-        ##
-        self._curr_bid_ask = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
+        self._cancels = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
             "BOND": [], "VALE": [], "VALBZ": []}
 
-    def update_buy(self, buysignal):
-        for _ in buysignal:
-            self._inventory[_] += buysignal[_]
-    def update_sell(self, sellsignal):
-            self._inventory[_] -= sellsignal[_]
+        self._buyidx = 1
+        self._sellidx = 100000
 
+        self._curr_bids = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
+            "BOND": [], "VALE": [], "VALBZ": []}
+        self._curr_asks = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
+            "BOND": [], "VALE": [], "VALBZ": []}
+
+
+    def update_inventory(self, fill_strings):
+        ## go over the fill strings, delete the corresponding outstanding bids
+        for _ in fill_strings:
+            name, ord, stock, action = fill_strings[_].split()[0:4]
+            ord = int(ord)
+            if name == 'FILL' and action == 'BUY':
+                to_delete = []
+                # go over the bids and asks and cancel that one
+                for idx in range(len(self._curr_bids[stock]))
+                    if self._curr_bids[stock][idx][0] == ord
+                    to_delete.append(idx)
+                del self._curr_bids[stock][to_delete] # delte filled bids
+                self._inventory[stock] += self._quant
+
+            if name == 'FILL' and action == 'SELL':
+                to_delete = []
+                # go over the bids and asks and cancel that one
+                for idx in range(len(self._curr_asks[stock]))
+                    if self._curr_asks[stock][idx][0] == ord
+                    to_delete.append(idx)
+                del self._curr_asks[stock][to_delete] # delete filled asks
+                self._inventory[stock] -= self._quant # 
+
+         
     def get_avg_prices(self, books):
         # "book" is the book dictionary for this collection of stocks
         for _ in books:
@@ -106,65 +125,51 @@ class Market_Maker:
             length = len(books['buy'])
             for idx in range(length):
                 self._buyprices[key] += books[_]['buy'][idx][0]
-            self._buyprices[key] /= length
+            self._buyprices[key] //= length
 
             self._sellprices[key] = 0.0
             length = len(books['sell'])
             for idx in range(length):
                 self._sellprices[key] += books[_]['sell'][idx][0]
-            self._sellprices[key] /= length
+            self._sellprices[key] //= length
+
+    def purge(self):
+        for _ in self._stocks:
+            self._cancels[_] = []
+            # check if there are any existing stocks with outstanding
+            # bids or asks and cancel them
+            for order in self._curr_bids[_]:
+                self.cancels[_].append("CANCEL " + str(order[0]))
+            for order in self._curr_asks[_]:
+                self.cancels[_].append("CANCEL " + str(order[0]))
+
+            self._curr_bids = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
+                "BOND": [], "VALE": [], "VALBZ": []}
+            self._curr_asks = {"XLF": [], "GS": [], "MS": [], "WFC": [], \
+                "BOND": [], "VALE": [], "VALBZ": []}
+
+        return self._cancels
 
 
+    def update_orders(self): # dictionary of prices
 
-    def update(self): # dictionary of prices
-        diff_1 = 10 * prices["XLF"] - self._costConvETF - 3 * prices["BOND"] -\
-            2 * prices["GS"] - 3 * prices["MS"] - 2 * prices["WFC"]
-        diff_2 = 3 * prices["BOND"] + 2 * prices["GS"] + 3 * prices["MS"] + \
-            2 * prices["WFC"] - 10 * prices["XLF"] - self._costConvETF
+        for _ in self._stocks: # stock key
+            self._buyidx += 1
+            self._sellidx += 1
+            # update buy and sell orders
+            self._buy[_] = "ADD " + self._buyidx + " " + key + " BUY " + \
+                str(sell._buyprices[_])+" " + self._quant
+            self._sell[_] = "ADD " + self.buyidx + " " + key + " SELL " + \
+                str(sell._sellprices[_])+" " + self._quant
 
-        # update time
-        self._curent_time = int(round(time.time()))
+             if self._inventory[_] < self._limits[_] - self._quant:
+                 self._curr_bids[_].append([self._buyidx, sell._buyprices[_], \
+                     self._quant])
+             if self._inventory[_] > -self._limits[_] + self._quant
+                 self._curr_asks[_].append([self._sellidx, sell._sellprices[_],\
+                     self._quant])
 
-        if diff_1 > 0.0:
-            quant_MS = (100 - self.inventory["MS"]) // 3
-            quant_GS = (100 - self.inventory["GS"]) // 2
-            quant_BOND = (100 - self.inventory["BOND"]) // 3
-            quant_WFC = (100 - self.inventory["WFC"]) // 2
-            quant_XLF = (100 + self.inventory["XLF"]) // 10
-            quant = min(quant_MS,quant_GS,quant_BOND,quant_WFC,quant_XLF)
-
-            self._buyETF = {"XLF": 0, "GS": 2*quant, "MS": 3*quant,
-                "WFC": 2*quant, "BOND": 3*quant}
-            self._sellETF = {"XLF":10*quant,"GS":0,"MS":0,"WFC":0,"BOND":0}
-            self._inventory["XLF"] -= 10 * quant
-            self._inventory["GS"] += 2*quant
-            self._inventory["BOND"] += 3*quant
-            self._inventory["WFC"] += 2*quant
-            self._inventory["MS"] += 3*quant
-
-        if diff_2 > 0.0:
-            quant_MS = (100 + self.inventory["MS"]) // 3
-            quant_GS = (100 + self.inventory["GS"]) // 2
-            quant_BOND = (100 + self.inventory["BOND"]) // 3
-            quant_WFC = (100 + self.inventory["WFC"]) // 2
-            quant_XLF = (100 - self.inventory["XLF"]) // 10
-            quant = min(quant_MS, quant_GS, quant_BOND, quant_WFC,quant_XLF)
-
-            self._sellETF = {"XLF": 0, "GS": 2*quant, "MS": 3*quant,
-                "WFC": 2*quant, "BOND": 3*quant}
-            self._buyETF = {"XLF":10*quant,"GS":0,"MS":0,"WFC":0,"BOND":0}
-
-            self._inventory["XLF"] += 10 * quant
-            self._inventory["GS"] -= 2*quant
-            self._inventory["BOND"] -= 3*quant
-            self._inventory["WFC"] -= 2*quant
-            self._inventory["MS"] -= 3*quant
-
-         # BE CAREFUL, WHEN BUYING XLF, THE SYNTAX MUST BE "CONVERT"
-         # NOT "BUY"
-
-        return self._buyETF,self._sellETF
-
+        return self._buy,self._sell
 
 
 class ConvTrade:
