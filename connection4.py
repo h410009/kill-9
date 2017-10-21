405887497a9c42fb093e231996c6e95d7ba5e6b1
FrozenWinters <tkolomatski@hotmail.com>
2017-10-21 15:14:16 +0100

    adf

--- connection4.py
+++ connection4.py
@@ -17,13 +17,13 @@ from random import random
 team_name="orange"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = False
+test_mode = True
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
 # 1 is slower
 # 2 is empty
-test_exchange_index=2
+test_exchange_index=0
 prod_exchange_hostname="production"
 
 port=25000 + (test_exchange_index if test_mode else 0)
@@ -103,13 +103,13 @@ def main():
             if (msg["symbol"] == "VALE"):
                 VALE = split[msg["buy"], msg["sell"]]
                 if len(VALEZ):
-                    if do_VALE(*VALE, *VALEZ):
+                    if do_VALE(VALE[0],VALE[1], VALEZ[0],VALEZ[1]):
                         VALE = []
                         VALEZ = []
             if (msg["symbol"] == "VALEZ"):
                 VALEZ = split[msg["buy"], msg["sell"]]
                 if len(VALE):
-                    if do_VALE(*VALE, *VALEZ):
+                    if do_VALE(VALE[0],VALE[1], VALEZ[0],VALEZ[1]):
                         VALE = []
                         VALEZ = []
 
